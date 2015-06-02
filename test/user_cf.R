31cb0ee2178b492513f437ff8bf1dd5d10a432ed
shaochunchang <shaochunchang@rd.tuan800.com>
2015-06-02 12:50:24 +0800

    添加userCf 对比测试

---
+++ test/user_cf.R
@@ -0,0 +1,27 @@
+getRecMatrix<-function(){
+  userCf<-read.csv("userCf.csv",header = FALSE)
+  names(userCf)<-c("userId","itemId","score")
+  userCf$userId<-as.character(userCf$userId)
+  userCf$itemId<-as.character(userCf$itemId)
+  users<-unique(userCf$userId)
+  items<-unique(userCf$itemId)
+  users_rownum<-1:length(users)
+  names(users_rownum)<-users
+  items_rownum<-1:length(items)
+  names(items_rownum)<-items
+  mx<-matrix(0,nrow = length(users),ncol = length(items),dimnames = (list(users,items)))
+  #使用坐标计算，将二维的坐标转换为一维的坐标
+  #一维的坐标可以按点取值，二维的只能按行按列取
+  mx[(items_rownum[userCf$itemId]-1)*length(users_rownum)+users_rownum[userCf$userId]]<-userCf$score
+  
+  similarity<-mx%*%t(mx)
+  s<-rowSums(mx^2)
+  #归一化
+  similarity<-similarity/sqrt(s%*%t(s))
+  similarity
+  
+  rec<-t(t(mx)%*%similarity)
+  rec[(items_rownum[userCf$itemId]-1)*length(users_rownum)+users_rownum[userCf$userId]]<-0
+  rec
+}
+
