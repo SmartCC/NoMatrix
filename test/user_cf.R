d4fb8adfd0b4ee6efa86556cfb59058faef880be
SmartCC <haoranqi@yeah.net>
2015-12-30 04:45:05 +0800

    分离距离计算公式

--- test/user_cf.R
+++ test/user_cf.R
@@ -1,3 +1,18 @@
+#计算矩阵相似性，最后一个参数为距离计算公式
+matrixVecComputerFun<-function(a,b,disFun){
+  if(nrow(a)!=ncol(b)){
+    stop("行列不一致，请检查矩阵！")
+  }
+  
+  apply(a,1,function(y) {apply(b,2,disFun,y)})
+}
+
+#使用欧几里得距离计算
+euclideanDistanceSimilarity<-function(x,y){
+  t<-y-x
+  length(t[t!=0])/(1+sqrt(sum(t^2)))
+}
+
 getRecMatrix<-function(){
     userCf<-read.csv("../data/userCf.csv",header = FALSE)
   names(userCf)<-c("userId","itemId","score")
@@ -13,14 +28,17 @@ getRecMatrix<-function(){
   #使用坐标计算，将二维的坐标转换为一维的坐标
   #一维的坐标可以按点取值，二维的只能按行按列取
   mx[(items_rownum[userCf$itemId]-1)*length(users_rownum)+users_rownum[userCf$userId]]<-userCf$score
-  
-  similarity<-mx%*%t(mx)
-  s<-rowSums(mx^2)
+
+  #user_similarity<-mx%*%t(mx)
+  #s<-rowSums(mx^2)
   #归一化
-  similarity<-similarity/sqrt(s%*%t(s))
-  similarity
+  #user_similarity<-user_similarity/sqrt(s%*%t(s))
+  user_similarity<-matrixVecComputerFun(mx,t(mx),euclideanDistanceSimilarity)
   
-  rec<-t(t(mx)%*%similarity)
+  #rec<-t(t(mx)%*%user_similarity)
+  #根据矩阵转置的性质，(A'B)'=B'A，又因为B是对称矩阵，B'=B
+  #故(A'B)'=BA
+  rec<-user_similarity%*%mx
   rec[(items_rownum[userCf$itemId]-1)*length(users_rownum)+users_rownum[userCf$userId]]<-0
   rec
 }
